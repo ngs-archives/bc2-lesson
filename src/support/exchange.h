4bb24a806d7c89993c2267ebe429a1da71f153fd
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-01-20 12:46:00 +0900

    Removed exchange stuff.

--- src/support/exchange.h
+++
@@ -1,8 +0,0 @@
-#ifndef BITCOIN_SRC_SUPPORT_EXCHANGE_H_
-#define BITCOIN_SRC_SUPPORT_EXCHANGE_H_
-
-#include <string>
-
-double GetExchangeRate(std::string currencyCode);
-
-#endif // BITCOIN_SRC_SUPPORT_EXCHANGE_H_
